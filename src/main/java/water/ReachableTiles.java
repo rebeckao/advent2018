e0836923a3563bdb8291ca31386c0deb640b772c
rebeckao <rebecka.oefverholm@gmail.com>
2018-12-18 22:53:25 +0100

    Day 17, underground water

--- src/main/java/water/ReachableTiles.java
+++ src/main/java/water/ReachableTiles.java
@@ -3,6 +3,10 @@ package water;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,92 +17,145 @@ import java.util.stream.Stream;
 class ReachableTiles {
     private static final Pattern VERTICAL_RESERVOIR = Pattern.compile("x=(\\d+), y=(\\d+)\\.\\.(\\d+)");
     private static final Pattern HORIZONTAL_RESERVOIR = Pattern.compile("y=(\\d+), x=(\\d+)\\.\\.(\\d+)");
+    private static final char CLAY = '%';
+    private static final char FLOWING = '|';
+    private static final char SETTLED = '~';
 
-    int tilesReachedByWater(Stream<String> coordinates) {
+    public static void main(String[] args) {
+        String fileName = "./src/main/resources/day17_clay.txt";
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            ReachableTiles reachableTiles = new ReachableTiles();
+            char[][] filled = reachableTiles.fillWithWater(stream);
+            System.out.println("Reached: " + reachableTiles.tilesReachedByWater(filled));
+            System.out.println("Filled: " + reachableTiles.tilesFilledByWater(filled));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    char[][] fillWithWater(Stream<String> coordinates) {
         Set<Coordinate> clayCoordinates = coordinates
                 .flatMap(this::parseCoordinates)
                 .collect(Collectors.toSet());
-        int minX = clayCoordinates.stream().mapToInt(Coordinate::getX).min().orElseThrow(() -> new IllegalStateException("Does not compute"));
+        int minX = clayCoordinates.stream().mapToInt(Coordinate::getX).min().orElseThrow(() -> new IllegalStateException("Does not compute")) - 1;
         int minY = clayCoordinates.stream().mapToInt(Coordinate::getY).min().orElseThrow(() -> new IllegalStateException("Does not compute"));
-        int maxX = clayCoordinates.stream().mapToInt(Coordinate::getX).max().orElseThrow(() -> new IllegalStateException("Does not compute"));
+        int maxX = clayCoordinates.stream().mapToInt(Coordinate::getX).max().orElseThrow(() -> new IllegalStateException("Does not compute")) + 1;
         int maxY = clayCoordinates.stream().mapToInt(Coordinate::getY).max().orElseThrow(() -> new IllegalStateException("Does not compute"));
-        char[][] map = new char[maxY - minY + 1][maxX - minX + 1];
-        clayCoordinates.forEach(coordinate -> map[coordinate.getY() - minY][coordinate.getX() - minX] = '#');
+        char[][] map = toNormalizedMatrix(clayCoordinates, minX, minY, maxX, maxY);
         int xCoordinateOfSpring = 500 - minX;
 
-        map[0][xCoordinateOfSpring] = '|';
-        char[][] newMap = new char[map.length][map[0].length];
-        while (newMap != map) {
-            System.arraycopy( map, 0, map, 0, map.length);
-            for (int y = 0; y < map.length - 1; y++) {
-                char[] row = map[y];
-                for (int x = 0; x < row.length; x++) {
-                    char square = row[x];
-                    if (square == '|') {
-                        char below = map[y + 1][x];
-                        if (below == '|') {
-                            continue;
-                        }
-                        if (below == 0) {
-                            map[y + 1][x] = '|';
-                            continue;
-                        }
-                        int wallToTheLeft = flowLeft(map, x, y);
-                        int wallToTheRight = flowRight(map, x, y);
-                        if (wallToTheLeft != -1 && wallToTheRight != -1) {
-                            for (int fillX = wallToTheLeft + 1; fillX < wallToTheRight; fillX++) {
-                                map[y][fillX] = '~';
-                            }
+        map[0][xCoordinateOfSpring] = FLOWING;
+        return fillWithWater(map);
+    }
+
+    char[][] fillWithWater(char[][] map) {
+        int turns = 0;
+        while (true) {
+//            System.out.println("After " + turns + " turns:");
+//            display(map);
+            turns++;
+            char[][] newMap = clone(map);
+            flow(newMap);
+            if (Arrays.deepEquals(map, newMap)) {
+                System.out.println(turns);
+                display(map);
+                return map;
+            }
+            map = newMap;
+        }
+    }
+
+    int tilesReachedByWater(char[][] map) {
+        int tilesReached = 0;
+        for (char[] row : map) {
+            for (char square : row) {
+                if (square == FLOWING || square == SETTLED) {
+                    tilesReached++;
+                }
+            }
+        }
+        return tilesReached;
+    }
+
+    int tilesFilledByWater(char[][] map) {
+        int tilesReached = 0;
+        for (char[] row : map) {
+            for (char square : row) {
+                if (square == SETTLED) {
+                    tilesReached++;
+                }
+            }
+        }
+        return tilesReached;
+    }
+
+    private void flow(char[][] map) {
+        for (int y = 0; y < map.length - 1; y++) {
+            char[] row = map[y];
+            for (int x = 0; x < row.length; x++) {
+                char square = row[x];
+                if (square == FLOWING) {
+                    char below = map[y + 1][x];
+                    if (below == FLOWING) {
+                        continue;
+                    }
+                    if (below == 0 || below == '.') {
+                        map[y + 1][x] = FLOWING;
+                        continue;
+                    }
+                    int wallToTheLeft = flowLeft(map, x, y);
+                    int wallToTheRight = flowRight(map, x, y);
+                    if (wallToTheLeft != -1 && wallToTheRight != -1) {
+                        for (int fillX = wallToTheLeft + 1; fillX < wallToTheRight; fillX++) {
+                            map[y][fillX] = SETTLED;
                         }
                     }
                 }
             }
         }
-        display(map);
+    }
 
-        return 0;
+    private char[][] toNormalizedMatrix(Set<Coordinate> clayCoordinates, int minX, int minY, int maxX, int maxY) {
+        char[][] map = new char[maxY - minY + 1][maxX - minX + 1];
+        clayCoordinates.forEach(coordinate -> map[coordinate.getY() - minY][coordinate.getX() - minX] = CLAY);
+        return map;
+    }
+
+    private char[][] clone(char[][] original) {
+        int length = original.length;
+        char[][] target = new char[length][original[0].length];
+        for (int i = 0; i < length; i++) {
+            System.arraycopy(original[i], 0, target[i], 0, original[i].length);
+        }
+        return target;
     }
 
     private int flowLeft(char[][] map, int x, int y) {
-        int stepsLeft = 1;
-        while (x - stepsLeft >= 0) {
-            char current = map[y][x - stepsLeft];
-            if (current == '|') {
-                System.out.println("Already flowing");
-                return -1;
-            }
-            char below = map[y + 1][x - stepsLeft];
-            if (current != '#') {
-                map[y][x - stepsLeft] = '|';
-                if (below == '#' || below == '~') {
-                    return x - stepsLeft; //Bounding wall
-                } else {
-                    return -1;
-                }
-            }
-            stepsLeft++;
-        }
-        return -1;
+        return flowInDirection(map, x, y, -1);
     }
 
     private int flowRight(char[][] map, int x, int y) {
-        int stepsRight = 1;
-        while (x + stepsRight < map[y].length) {
-            char current = map[y][x + stepsRight];
-            if (current == '|') {
-                System.out.println("Already flowing");
-                return -1;
-            }
-            char below = map[y + 1][x + stepsRight];
-            if (current != '#') {
-                map[y][x + stepsRight] = '|';
-                if (below == '#' || below == '~') {
-                    return x + stepsRight; //Bounding wall
+        return flowInDirection(map, x, y, 1);
+    }
+
+    private int flowInDirection(char[][] map, int x, int y, int direction) {
+        int newX = x + direction;
+        while (newX >= 0 && newX < map[y].length) {
+            char current = map[y][newX];
+            char below = map[y + 1][newX];
+            if (current == CLAY) {
+                if (below == CLAY || below == SETTLED) {
+                    return newX; //Bounding wall
                 } else {
                     return -1;
                 }
+            } else {
+                map[y][newX] = FLOWING;
+                if (below != CLAY && below != SETTLED) {
+                    return -1;
+                }
             }
-            stepsRight++;
+            newX += direction;
         }
         return -1;
     }
--- src/test/java/water/ReachableTilesTest.java
+++ src/test/java/water/ReachableTilesTest.java
@@ -1,17 +1,21 @@
 package water;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ReachableTilesTest {
     private ReachableTiles reachableTiles = new ReachableTiles();
 
     @Test
     void tilesReachedByWater() {
-        int actual = reachableTiles.tilesReachedByWater(Stream.of(
+        char[][] filled = reachableTiles.fillWithWater(Stream.of(
                 "x=495, y=2..7",
                 "y=7, x=495..501",
                 "x=501, y=3..7",
@@ -21,6 +25,95 @@ class ReachableTilesTest {
                 "x=504, y=10..13",
                 "y=13, x=498..504"
         ));
+        int actual = reachableTiles.tilesReachedByWater(filled);
         assertEquals(57, actual);
+    }
+    @Test
+    void tilesFilledByWater() {
+        char[][] filled = reachableTiles.fillWithWater(Stream.of(
+                "x=495, y=2..7",
+                "y=7, x=495..501",
+                "x=501, y=3..7",
+                "x=498, y=2..4",
+                "x=506, y=1..2",
+                "x=498, y=10..13",
+                "x=504, y=10..13",
+                "y=13, x=498..504"
+        ));
+        int actual = reachableTiles.tilesFilledByWater(filled);
+        assertEquals(29, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "......|.......;" +
+                    "............%.;" +
+                    ".%..%.......%.;" +
+                    ".%..%..%......;" +
+                    ".%..%..%......;" +
+                    ".%.....%......;" +
+                    ".%.....%......;" +
+                    ".%%%%%%%......;" +
+                    "..............;" +
+                    "..............;" +
+                    "....%.....%...;" +
+                    "....%.....%...;" +
+                    "....%.....%...;" +
+                    "....%%%%%%%...," +
+
+                    "......|.......;" +
+                    "......|.....%.;" +
+                    ".%..%||||...%.;" +
+                    ".%..%~~%|.....;" +
+                    ".%..%~~%|.....;" +
+                    ".%~~~~~%|.....;" +
+                    ".%~~~~~%|.....;" +
+                    ".%%%%%%%|.....;" +
+                    "........|.....;" +
+                    "...|||||||||..;" +
+                    "...|%~~~~~%|..;" +
+                    "...|%~~~~~%|..;" +
+                    "...|%~~~~~%|..;" +
+                    "...|%%%%%%%|..",
+
+            "...........|............;" +
+                    ".%....................%.;" +
+                    ".%....................%.;" +
+                    ".%....................%.;" +
+                    ".%............%.%.....%.;" +
+                    ".%............%.%.....%.;" +
+                    ".%............%.%.....%.;" +
+                    ".%............%.%.....%.;" +
+                    ".%............%%%.....%.;" +
+                    ".%....................%.;" +
+                    ".%....................%.;" +
+                    ".%%%%%%%%%%%%%%%%%%%%%%.," +
+
+                    "||||||||||||||||||||||||;" +
+                    "|%~~~~~~~~~~~~~~~~~~~~%|;" +
+                    "|%~~~~~~~~~~~~~~~~~~~~%|;" +
+                    "|%~~~~~~~~~~~~~~~~~~~~%|;" +
+                    "|%~~~~~~~~~~~~%~%~~~~~%|;" +
+                    "|%~~~~~~~~~~~~%~%~~~~~%|;" +
+                    "|%~~~~~~~~~~~~%~%~~~~~%|;" +
+                    "|%~~~~~~~~~~~~%~%~~~~~%|;" +
+                    "|%~~~~~~~~~~~~%%%~~~~~%|;" +
+                    "|%~~~~~~~~~~~~~~~~~~~~%|;" +
+                    "|%~~~~~~~~~~~~~~~~~~~~%|;" +
+                    "|%%%%%%%%%%%%%%%%%%%%%%|"
+    })
+    void fill(String mapString, String expectedString) {
+        char[][] expectedMatrix = toCharMatrix(expectedString);
+        char[][] actual = reachableTiles.fillWithWater(toCharMatrix(mapString));
+        assertTrue(Arrays.deepEquals(expectedMatrix, actual));
+    }
+
+    private char[][] toCharMatrix(String beforeFill) {
+        String[] rows = beforeFill.split(";");
+        char[][] map = new char[rows.length][rows[0].length()];
+        for (int i = 0; i < rows.length; i++) {
+            map[i] = rows[i].toCharArray();
+        }
+        return map;
     }
 }
