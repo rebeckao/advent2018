206c9037728bf1b2c65fad41dec1fb9a6f82b12d
rebeckao <rebecka.oefverholm@gmail.com>
2018-12-19 20:44:56 +0100

    Day 18 part 2, lumbering

--- src/main/java/lumber/Lumbering.java
+++ src/main/java/lumber/Lumbering.java
@@ -5,7 +5,9 @@ import common.Util;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.stream.Collectors.toList;
 
@@ -22,9 +24,10 @@ class Lumbering {
                     .collect(toList());
             Lumbering lumbering = new Lumbering();
             char[][] initial = Util.toCharMatrix(lines);
-            char[][] after = lumbering.afterMinutes(initial, 10);
-//            char[][] after = lumbering.afterMinutes(initial, 1000000000);
-            System.out.println(lumbering.resourceValue(after));
+            char[][] afterTen = lumbering.afterMinutes(initial, 10);
+            System.out.println(lumbering.resourceValue(afterTen));
+            char[][] afterABillion = lumbering.afterMinutes(initial, 1000000000);
+            System.out.println(lumbering.resourceValue(afterABillion));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,44 +45,79 @@ class Lumbering {
                 }
             }
         }
-        return trees*lumberyards;
+        return trees * lumberyards;
     }
 
-    char[][] afterMinutes(char[][] state, int minutes) {
-        for (int i = 0; i < minutes; i++) {
+    char[][] afterMinutes(char[][] state, long minutes) {
+        Map<Integer, Long> hashes = new HashMap<>();
+        boolean cycleDetected = false;
+        for (long i = 0; i < minutes; i++) {
+//            System.out.println(i);
 //            Util.display(state);
             char[][] newState = Util.clone(state);
             int rows = state.length;
             int columns = state[0].length;
             for (int row = 0; row < rows; row++) {
                 for (int column = 0; column < columns; column++) {
-                    char current = state[row][column];
-                    int trees = 0;
-                    int lumberyards = 0;
-                    for (int adjRow = Math.max(0, row - 1); adjRow <= Math.min(row + 1, rows - 1); adjRow++) {
-                        for (int adjCol = Math.max(0, column - 1); adjCol <= Math.min(column + 1, columns - 1); adjCol++) {
-                            if (adjRow == row && adjCol == column) {
-                                continue;
-                            }
-                            char adjecent = state[adjRow][adjCol];
-                            if (adjecent == TREE) {
-                                trees++;
-                            } else if (adjecent == LUMBERYARD) {
-                                lumberyards++;
-                            }
-                        }
-                    }
-                    if (current == OPEN) {
-                        newState[row][column] = trees >= 3 ? TREE : OPEN;
-                    } else if (current == TREE) {
-                        newState[row][column] = lumberyards >= 3 ? LUMBERYARD : TREE;
-                    } else {
-                        newState[row][column] = lumberyards >= 1 && trees >= 1 ? LUMBERYARD : OPEN;
-                    }
+                    newState[row][column] = resolveNewValue(state, rows, columns, row, column);
+                }
+            }
+            if (!cycleDetected) {
+                int hash = hash(state);
+                if (hashes.containsKey(hash)) {
+                    minutes = shortenMinutes(minutes, hashes.get(hash), i);
+                    cycleDetected = true;
+                } else {
+                    hashes.put(hash, i);
                 }
             }
             state = newState;
         }
         return state;
+    }
+
+    private long shortenMinutes(long minutes, long previousEqualTurn, long currentTurn) {
+        System.out.println("Cycle detected! turn " + currentTurn + " == turn " + previousEqualTurn);
+        long cycleLength = currentTurn - previousEqualTurn;
+        long remainingMinutes = minutes - currentTurn;
+        long actualRemainingMinutes = remainingMinutes % cycleLength;
+        System.out.println("Shortening remaining minutes from " + remainingMinutes + " to " + actualRemainingMinutes);
+        return currentTurn + actualRemainingMinutes;
+    }
+
+    private char resolveNewValue(char[][] state, int rows, int columns, int row, int column) {
+        char current = state[row][column];
+        int trees = 0;
+        int lumberyards = 0;
+        for (int adjRow = Math.max(0, row - 1); adjRow <= Math.min(row + 1, rows - 1); adjRow++) {
+            for (int adjCol = Math.max(0, column - 1); adjCol <= Math.min(column + 1, columns - 1); adjCol++) {
+                if (adjRow == row && adjCol == column) {
+                    continue;
+                }
+                char adjecent = state[adjRow][adjCol];
+                if (adjecent == TREE) {
+                    trees++;
+                } else if (adjecent == LUMBERYARD) {
+                    lumberyards++;
+                }
+            }
+        }
+        if (current == OPEN) {
+            return trees >= 3 ? TREE : OPEN;
+        } else if (current == TREE) {
+            return lumberyards >= 3 ? LUMBERYARD : TREE;
+        } else {
+            return lumberyards >= 1 && trees >= 1 ? LUMBERYARD : OPEN;
+        }
+    }
+
+    private int hash(char[][] map) {
+        int hash = 0;
+        for (int row = 0; row < map.length; row++) {
+            for (int column = 0; column < map[0].length; column++) {
+                hash += row * column * map[row][column];
+            }
+        }
+        return hash;
     }
 }
